92613fe9a545dbd586cd20a77a8c3c019f69af04
MichalFidor <michal.fidor@testarmy.com>
2019-03-21 10:08:30 +0100

    changing images

--- config/config.ts
+++ config/config.ts
@@ -43,7 +43,7 @@ export const config: Config = {
             {
                 baselineFolder: 'data/image-comparison/baseline/',
                 screenshotPath: 'data/image-comparison/screenshots/',
-                formatImageName: '{browserName}_{tag}_{width}-{height}',
+                formatImageName: '{browserName}_{tag}',
                 autoSaveBaseline: true,
                 canvasScreenshot: true
             }
